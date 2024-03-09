60ca9d27b7e56b83bc685691f0ff2723cd72d1c0
Temhithorphe <Tehmhithorphe@gmail.com>
2024-03-09 19:23:05 +0100

    [Added]: New assets & base css reset.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+// Path: next.config.js
+const nextConfig = {
+    webpack: config => {
+        config.externals.push('pino-pretty', 'lokijs', 'encoding')
+        return config
+    }
+}
 export default nextConfig;
