8394fe32d3c4144f39845ef692decbb08aebc448
Temhithorphe <Tehmhithorphe@gmail.com>
2024-03-10 11:13:42 +0100

    [Added]: Swap Card assets

---
+++ src/app/components/BinanceWalletSVG.jsx
@@ -0,0 +1,33 @@
+
+
+
+export function BinanceWalletSVG() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            fill="none"
+            viewBox="0 0 40 40"
+        >
+            <path
+                fill="#14151A"
+                d="M0 8a8 8 0 018-8h24a8 8 0 018 8v24a8 8 0 01-8 8H8a8 8 0 01-8-8V8z"
+            ></path>
+            <path
+                fill="#F0B90B"
+                d="M6 20l3.161-3.161L12.323 20 9.16 23.161 6 20zM11.42 14.58L20 6l8.58 8.58-3.16 3.162-5.42-5.42-5.42 5.42-3.16-3.161z"
+            ></path>
+            <path
+                fill="#F0B90B"
+                d="M16.839 20L20 16.839 23.161 20 20 23.161 16.839 20z"
+            ></path>
+            <path
+                fill="#F0B90B"
+                d="M14.58 22.258l-3.16 3.161L20 34l8.58-8.58-3.16-3.162-5.42 5.42-5.42-5.42zM27.677 20l3.162-3.161L34 20l-3.161 3.161L27.677 20z"
+            ></path>
+        </svg>
+    );
+}
+
+export default BinanceWalletSVG
---
+++ src/app/components/TorrusWalletSVG.jsx
@@ -0,0 +1,18 @@
+
+
+const TorrusWalletSVG = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 257 277"
+        height="100%"
+        width="100%"
+    >
+        <path fill="#0364FF" d="M0 0h153.889v82.074H0z" />
+        <path
+            fill="#0364FF"
+            d="M71.814 0h82.074v277H71.814zM215.443 82.074c22.664 0 41.037-18.373 41.037-41.037C256.48 18.373 238.107 0 215.443 0c-22.664 0-41.037 18.373-41.037 41.037 0 22.664 18.373 41.037 41.037 41.037Z"
+        />
+    </svg>
+)
+export default TorrusWalletSVG
