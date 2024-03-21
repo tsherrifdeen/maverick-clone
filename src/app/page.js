390f30c3f24751a7bd5634911c1177daa0e9e485
Habib Kamil <tsherrifdeen@gmail.com>
2024-03-21 23:25:27 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,10 @@ import Footer from "./components/Footer";
 import Hero from "./components/Hero.jsx";
 import Nav from "./components/Nav.jsx";
 import GraphSection from "./components/graphSection.jsx";
-import CardSection from "./components/CardSection";
-import CommSection from "./components/CommSection";
-import EcoCard from "./components/EcoCard";
-import FooterAbout from "./components/FooterAbout";
+import CardSection from "./components/CardSection.jsx";
+import CommSection from "./components/CommSection.jsx";
+import EcoCard from "./components/EcoCard.jsx";
+import FooterAbout from "./components/FooterAbout.jsx";
 
 export default function Home() {
   return (
